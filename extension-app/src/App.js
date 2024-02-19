dc52509c111e7ce55fb400fd4412508fdb40f5f0
Asonjay <49351464+Asonjay@users.noreply.github.com>
2024-02-19 14:02:45 -0500

    02/19

---
+++ extension-app/src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import Generator from "./components/Generator";
+import Profile from "./components/Profile";
+import { ROUTES } from "./utils/routes";
+import { loadData } from "./utils/localStorage";
+
+function App() {
+	const [page, setPage] = useState();
+	const [resume, setResume] = useState("resume text here...");
+	const [openAIKey, setOpenAIKey] = useState("openAIKey text here...");
+
+	useEffect(() => {
+		const fetchLocalData = async () => {
+			const fetchedResume = await loadData("resume");
+			const fetchedOpenAIKey = await loadData("openAIKey");
+			setResume(fetchedResume);
+			setOpenAIKey(fetchedOpenAIKey);
+		};
+
+		fetchLocalData();
+	}, []);
+
+	switch (page) {
+		case ROUTES.GENERATOR:
+			return <Generator setPage={setPage} />;
+		case ROUTES.PROFILE:
+			return (
+				<Profile
+					setPage={setPage}
+					resume={resume}
+					setResume={setResume}
+					openAIKey={openAIKey}
+					setOpenAIKey={setOpenAIKey}
+				/>
+			);
+		default:
+			return <Generator setPage={setPage} />;
+	}
+}
+
+export default App;
